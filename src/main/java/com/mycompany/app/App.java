e8a3e3ec3485a77b702af9c6c50d84e13809cfa4
awsdevopsrealtime1 <awsdevopsrealtime1@gmail.com>
2025-07-14 10:47:08 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello DevOps!";
+    private static final String MESSAGE = "Hello AWS DevOps!";
 
     public App() {}
 
